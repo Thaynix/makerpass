706908b667ffd84bb42bc7e159da987a79ee41c0
Thaynix <thaygoncalvesjg@gmail.com>
2025-08-12 23:57:35 -0300

    WIP: adiciona estila na pagina principal

--- static/js/main.js
+++ static/js/main.js
@@ -6,9 +6,8 @@ function updateClock() {
         const now = new Date();
         const hours = String(now.getHours()).padStart(2, '0');
         const minutes = String(now.getMinutes()).padStart(2, '0');
-        const seconds = String(now.getSeconds()).padStart(2, '0');
         
-        clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+        clockElement.textContent = `${hours}:${minutes}`;
     }
 }
 
